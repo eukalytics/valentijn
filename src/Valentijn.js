c8ae106566deb3edcec4ec113d44bebdd37e279e
eukalytics <eukalytics@protonmail.com>
2022-02-14 17:43:45 +0000

    add confetti

--- src/Valentijn.js
+++ src/Valentijn.js
@@ -2,6 +2,8 @@ import Typing from "./component/Typing";
 import Grid from "./component/Grid";
 import Btn from "./component/Btn";
 import { useState } from "react";
+import Confetti from 'react-confetti'
+import { useWindowSize } from 'react-use';
 
 export default function Valentijn() {
   const [stage, setStage] = useState(1);
@@ -18,13 +20,25 @@ export default function Valentijn() {
     4: 'Happy Valentines Day! 🐨😘'
 }
 
-  const introText = "Blain Maguire, will you be my valentine?";
+const { width, height } = useWindowSize()
+
+  const confetti = <Confetti
+      recycle={false}
+      numberOfPieces={1000}
+      opacity={0.7}
+      initialVelocityY={-10}
+      height={height}
+      width={width}
+      colors={['#FF0000', '#FFFFFF', '#FFC0CB']}
+    />
+
 
   return (
     <div className="bg-black min-h-screen font-mono p-10">
         <Grid stage={stage}/>
         <Typing text={text[stage]} />
         {stage < 4 && <Btn incrStage={incrStage} /> }
+        {stage === 4 && confetti}
     </div>
   );
 }
